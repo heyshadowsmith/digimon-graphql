a582239792966e3d4341590d3998338d6019db72
Shadow Smith <hey@shadowsmith.com>
2021-07-27 22:22:27 -0500

    Add digimon to dataSources

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./typeDefs')
 const resolvers = require('./resolvers')
+const digimon = require('./digimon')
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  dataSources: () => ({
+    digimon
+  }),
   playground: true,
   introspection: true
 })
